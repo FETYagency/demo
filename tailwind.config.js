9abfd4533f7c6fe372e1ec443ce5dc109bd2c016
FETYagency <fetyagency@gmail.com>
2023-09-14 19:33:36 +0100

    forms added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
+    theme: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray:'#ecc94b',
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
